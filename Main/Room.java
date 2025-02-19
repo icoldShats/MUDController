b3f43d6f77854f5690c7fea239b5996e7e500fe5
icoldShats <medina.sagudenova@narxoz.kz>
2025-02-19 20:30:22 +0600

    new

--- Main/Room.java
+++ Main/Room.java
@@ -1,16 +1,43 @@
-public class Room implements IGameEntity{
+import java.util.*;
+class Room implements IGameEntity {
     private String name;
     private String description;
+    private Map<String, Room> exits = new HashMap<>();
+    private List<String> items = new ArrayList<>();
 
-    public Room(String name, String description){
-        this.name=name;
-        this.description=description;
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
-    public String getName(){
-        return name;
+
+    public void setExit(String direction, Room room) {
+        exits.put(direction, room);
     }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+    }
+
+    public boolean removeItem(String item) {
+        return items.remove(item);
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+
     @Override
-    public void describe(){
-        System.out.println("Room :" + name+ " - " + description);
+    public void describe() {
+        System.out.println(name + " - " + description);
+        if (!items.isEmpty()) {
+            System.out.println("You see: " + items);
+        }
+        if (!exits.isEmpty()) {
+            System.out.println("Exits: " + exits.keySet());
+        }
     }
 }
